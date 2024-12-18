fd547932cc427e29668effbe93cb8c16426cea03
Shananoha <x1390768297@outlook.com>
2024-12-18 11:15:22 +0800

    dataLoadRecode2

--- common steps/dataLoadRecode.m
+++ common steps/dataLoadRecode.m
@@ -8,13 +8,6 @@ data = readmatrix('test.txt');
 nonNanIndices = all(~isnan(data), 2);
 data = data(nonNanIndices, :);
 
-% 检查数据维度
-if size(data, 2) ~= 2
-    error('Data must have two columns.');
-end
-
 % 提取x和y值
 X = data(:, 1);
 Y = data(:, 2);
-
-display(X);
